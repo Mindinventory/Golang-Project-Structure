f61c12011303b317b3b426591bb065112fd3e046
Kiran Malvi <kiran.mind2014@gmail.com>
2019-06-13 14:58:02 +0530

    Update helpers.go

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -4,9 +4,7 @@ import (
 	"strconv"
 )
 
-/*
-Int64ToString function convert a float number to a string
- */
+//Int64ToString function convert a float number to a string
 func Int64ToString(inputNum int64) string {
 		return strconv.FormatInt(inputNum, 10)
 }
