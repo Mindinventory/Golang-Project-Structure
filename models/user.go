a1725f21a866208891e45b06ce89825026920bcd
Kiran Malvi <kiran.mind2014@gmail.com>
2019-06-13 15:04:50 +0530

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,17 +1,13 @@
 package models
 
-/*
-User structure
- */
+//User structure
 type User struct {
 	Model
 	Name  string `gorm:"type:varchar(50)" json:"name" validate:"required"`
 	Email string `gorm:"type:varchar(50)" json:"email" validate:"required,email"`
 }
 
-/*
-TableName return name of database table
- */
+//TableName return name of database table
 func (u *User) TableName() string {
 	return "user"
 }
